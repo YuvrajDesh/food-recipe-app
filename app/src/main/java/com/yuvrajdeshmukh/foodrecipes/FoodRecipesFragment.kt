1e9c5249cf0ae828d6a2a4d028065b0484d99b98
YuvrajDesh <yuvrajdeshmukh8998@gmail.com>
2021-12-06 01:32:00 +0530

    app is done well upto placeholder_row_layout

---
+++ app/src/main/java/com/yuvrajdeshmukh/foodrecipes/FoodRecipesFragment.kt
@@ -0,0 +1,25 @@
+package com.yuvrajdeshmukh.foodrecipes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_food_recipes.view.*
+
+
+class FoodRecipesFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_food_recipes, container, false)
+        view.shimmer_recycler_view.showShimmer()
+        return view
+    }
+
+
+    }
---
+++ app/src/main/java/com/yuvrajdeshmukh/foodrecipes/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.yuvrajdeshmukh.foodrecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController:NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var bottomNavigationView:BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+//
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+
+
+
+
+
+
+
+
+
+}
